7a5ce601d9c20a5249676dedd81ef7316e9f8938
Roman Batrakov <Kyct69@yandex.ru>
2024-02-21 22:01:08 +0300

    Added new toys in shop

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,13 @@ public class Main {
         toyShop.addToy(new Toy(1, "Кукла", 0.3));
         toyShop.addToy(new Toy(2, "Машинка", 0.2));
         toyShop.addToy(new Toy(3, "Мяч", 0.5));
+        toyShop.addToy(new Toy(4, "Пазлы", 0.4));
+        toyShop.addToy(new Toy(5, "Набор для творчества", 0.6));
+        toyShop.addToy(new Toy(6, "Конструктор", 0.7));
+        toyShop.addToy(new Toy(7, "Мягкие игрушки", 0.8));
+        toyShop.addToy(new Toy(8, "Музыкальные инструменты", 0.3));
+        toyShop.addToy(new Toy(9, "Спортивные игры", 0.4));
+        toyShop.addToy(new Toy(10, "Интерактивные игрушки", 0.6));
 
         // Проведение розыгрыша
         ToyDraw toyDraw = new ToyDraw();
